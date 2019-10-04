fb136ef33a5aea2969436519e41cfc45f529160d
Rebecca Hassett <rebecca.hassett1998@gmail.com>
2019-10-03 22:44:29 -0400

    Dependency Issues Fix Attempt

---
+++ src/components/Sidebar.js
@@ -0,0 +1,132 @@
+import React, {Component} from 'react';
+import styled from "styled-components";
+import $ from 'jquery';
+import {Col} from "react-bootstrap";
+
+export default class Sidebar extends Component {
+    render() {
+        return (
+            <Sidebar_Style>
+            <div className="wrapper">
+                <nav id="sidebar">
+                    <div className="sidebar-header">
+                        <h3>Bootstrap Sidebar</h3>
+                    </div>
+
+                    <ul className="list-unstyled components">
+                        <p>Dummy Heading</p>
+                        <li className="active">
+                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false"
+                               className="dropdown-toggle">Home</a>
+                            <ul className="collapse list-unstyled" id="homeSubmenu">
+                                <li>
+                                    <a href="#">Home 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
+                               className="dropdown-toggle">Pages</a>
+                            <ul className="collapse list-unstyled" id="pageSubmenu">
+                                <li>
+                                    <a href="#">Page 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </div>
+            </Sidebar_Style>
+
+        );
+    }
+}
+
+const Sidebar_Style = styled.div`
+ @import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+
+
+body {
+    font-family: 'Poppins', sans-serif;
+    background: #fafafa;
+}
+
+p {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.1em;
+    font-weight: 300;
+    line-height: 1.7em;
+    color: #999;
+}
+
+a, a:hover, a:focus {
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.3s;
+}
+
+#sidebar {
+    /* don't forget to add all the previously mentioned styles here too */
+    background: #7386D5;
+    color: #fff;
+    transition: all 0.3s;
+}
+
+#sidebar .sidebar-header {
+    padding: 20px;
+    background: #6d7fcc;
+}
+
+#sidebar ul.components {
+    padding: 20px 0;
+    border-bottom: 1px solid #47748b;
+}
+
+#sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+}
+#sidebar ul li a:hover {
+    color: #7386D5;
+    background: #fff;
+}
+
+#sidebar ul li.active > a, a[aria-expanded="true"] {
+    color: #fff;
+    background: #6d7fcc;
+}
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #6d7fcc;
+}
+`;
+
