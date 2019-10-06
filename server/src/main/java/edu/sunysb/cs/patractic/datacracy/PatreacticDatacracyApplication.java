7f5565452d40623d9ab08c931a47fa61a513c0d3
Andrew Jaffie <andrew@ajaffie.dev>
2019-10-06 13:49:30 -0400

    Add initial server

---
+++ server/src/main/java/edu/sunysb/cs/patractic/datacracy/PatreacticDatacracyApplication.java
@@ -0,0 +1,13 @@
+package edu.sunysb.cs.patractic.datacracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatreacticDatacracyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatreacticDatacracyApplication.class, args);
+    }
+
+}
