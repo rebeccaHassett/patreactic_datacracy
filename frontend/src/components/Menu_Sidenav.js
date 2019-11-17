964fbe7c10137aafd85024a4004d6d26d9420d52
Rebecca Hassett <rebecca.hassett1998@gmail.com>
2019-11-16 21:57:11 -0500

    Fixed inconsistencies mapping election data with rhode island and michigan

--- frontend/src/components/Menu_Sidenav.js
+++ frontend/src/components/Menu_Sidenav.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { bubble as Menu } from 'react-burger-menu'
+import {bubble as Menu} from 'react-burger-menu'
 import {Button, Row, Tab, Tabs, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import Controls from "./Controls";
 import styled from "styled-components";
+import Slider_Controls from "./Slider_Control";
 
 
 class Menu_Sidenav extends React.Component {
@@ -20,41 +21,59 @@ class Menu_Sidenav extends React.Component {
             Sidebar: true
         }
     }
-    showSettings (event) {
+
+    showSettings(event) {
         event.preventDefault();
     }
 
-render () {
+    render() {
         return (
-            <Menu styles={ styles } disableCloseOnEsc noOverlay left isOpen={true}>
+            <Menu styles={styles} disableCloseOnEsc noOverlay left isOpen={true}>
                 <Content_Style>
-                <Tabs defaultActiveKey="menu" id="tabs">
-                    <Tab id="menu" eventKey="menu" title="Menu" disabled={this.state.Tab6}>
-                        <Row>
-                            <Link to='/map'>
-                                <Button>Back to US Map</Button>
-                            </Link>
-                        </Row>
-                        <Row>
-                            <Button>Start</Button>
-                            <Form>
-                                <Form.Group controlId="incremental" id="incremental">
-                                    <Form.Label>Incremental Run:</Form.Label>
-                                    <Form.Check aria-label="option 1" label="Incremental"/>
-                                </Form.Group>
-                            </Form>
-                        </Row>
-                        <Row>
-                            <Button disabled>Toggle to Original Districts</Button>
-                        </Row>
-                    </Tab>
-                    <Tab eventKey="phase0" title="Phase 0">
-                        <h1>Phase 0 Controls</h1>
-                    </Tab>
-                <Tab eventKey="controls" title="Controls" disabled={this.state.Tab5}>
-                    <Controls></Controls>
-                </Tab>
-            </Tabs>
+                    <Tabs defaultActiveKey="menu" id="tabs">
+                        <Tab id="menu" eventKey="menu" title="Menu" disabled={this.state.Tab6}>
+                            <Row>
+                                <Link to='/map'>
+                                    <Button>Back to US Map</Button>
+                                </Link>
+                            </Row>
+                            <Row>
+                                <Button>Start</Button>
+                                <Form>
+                                    <Form.Group controlId="incremental" id="incremental">
+                                        <Form.Label>Incremental Run:</Form.Label>
+                                        <Form.Check aria-label="option 1" label="Incremental"/>
+                                    </Form.Group>
+                                </Form>
+                            </Row>
+                            <Row>
+                                <Button disabled>Toggle to Original Districts</Button>
+                            </Row>
+                        </Tab>
+                        <Tab eventKey="phase0" title="Phase 0">
+                            <Form.Group controlId="bloc-population-percentage">
+                                <Form.Label>Block Population Percentage Threshold:</Form.Label>
+                                <Slider_Controls></Slider_Controls>
+                            </Form.Group>
+                            <Form.Group controlId="bloc-voting-percentage">
+                                <Form.Label>Block Voting Percentage Threshold:</Form.Label>
+                                <Slider_Controls></Slider_Controls>
+                            </Form.Group>
+                            <Form.Group controlId="election-types">
+                                <Form.Label>Election Type</Form.Label>
+                                <Form.Check aria-label="option 1" label="Congressional 2016"/>
+                                <Form.Check aria-label="option 2" label="Congressional 2018"/>
+                                <Form.Check aria-label="option 3" label="Presidential 2016"/>
+                            </Form.Group>
+                            <Button>Start Phase 0</Button>
+                        </Tab>
+                        <Tab eventKey="phase1" title="Phase 1" disabled={this.state.Tab5}>
+                            <Controls></Controls>
+                        </Tab>
+                        <Tab eventKey="phase2" title="Phase 2" disabled={this.state.Tab5}>
+                            <Controls></Controls>
+                        </Tab>
+                    </Tabs>
                 </Content_Style>
             </Menu>
         );
@@ -64,6 +83,9 @@ render () {
 const Content_Style = styled.div`
     * {
         outline:none;
+    }
+    Form {
+        color:lightblue;
     }
     #incremental {
         color:lightblue;
@@ -75,6 +97,7 @@ const Content_Style = styled.div`
         color:black;
         font-style:bold;
         border-color:black;
+        margin-left: 2vw;
     }
 
     Tabs {
@@ -102,11 +125,11 @@ var styles = {
     bmBurgerBarsHover: {
         background: '#a90000',
 
-},
+    },
     bmCrossButton: {
         height: '3vw',
         width: '3vw',
-        left:'1vw'
+        left: '1vw'
     },
     bmCross: {
         background: '#bdc3c7'
@@ -119,18 +142,19 @@ var styles = {
         background: '#373a47',
         padding: '1.0em 1.0em 0',
         fontSize: '1.15em',
-        outline:'none'
+        outline: 'none',
+        width: '30vw'
     },
     bmMorphShape: {
         fill: '#373a47'
     },
     bmItemList: {
         color: '#b8b7ad',
-        outline:'none !important',
+        outline: 'none !important',
         padding: '0.8em'
     },
     btmItem: {
-        outline:'none'
+        outline: 'none'
     },
 
     bmOverlay: {
--- updated_preprocessing/map_election_geographic_data.py
+++ updated_preprocessing/map_election_geographic_data.py
@@ -1,17 +1,45 @@
 import json
 import argparse
 import pandas as pd
+import regex as re
 
 '''State Options: 1 = Rhode Island, 2 = Michigan, 3 = North Carolina'''
+
+
+
+def fix_wards_precincts(precinct):
+    if precinct.find(" Ward ") != -1:
+        first_part = precinct.split(" Ward ")[0]
+        precinct_id = re.search(r'\d+', first_part).group()
+        ward_id = precinct.split(" Ward ")[1]
+        first_part_no_digit = first_part.split(precinct_id)[0]
+        final_precinct = first_part_no_digit + "Ward " + ward_id + " Precinct " + precinct_id
+        return final_precinct
+    else:
+        return precinct
+
+def print_unmapped_features(features, year):
+    for feature in features:
+        if feature["properties"][year] == {}:
+            f = open("unmapped.txt", "a")
+            f.write("UNMAPPED PRECINCT: " + feature["properties"]["PRENAME"] + "\n")
+            f.close()
 
 
 # for each precinct
 #   query election data by precinct name and update
 #       when update year + _candidate + _party = key and total votes = value
-def update_precinct_election_data(year, election_list, precinct_features):
+def update_precinct_election_data(year, election_list, precinct_features, split_precinct_identifier):
     for precinct_index, precinct in enumerate(precinct_features):
-        print("Precinct: " + str(precinct_index))
-        election_data = election_list[election_list.precinct == precinct["properties"]["PRENAME"]]
+        if precinct_index % 100 == 0:
+            print("Precinct: " + str(precinct_index))
+        if split_precinct_identifier:
+            parent_jurisdiction = precinct["properties"]["PRENAME"].split(" ")[0]
+            same_parent_jurisdiction = election_list[election_list.parent_jurisdiction == parent_jurisdiction]
+            jurisdiction = precinct["properties"]["PRENAME"].split(" ")[1].lstrip("0")
+            election_data = same_parent_jurisdiction[same_parent_jurisdiction.jurisdiction == jurisdiction]
+        else:
+            election_data = election_list[election_list.precinct == precinct["properties"]["PRENAME"].upper().replace('|', '')]
         election = {}
         for index, row in election_data.iterrows():
             candidate = row["candidate"]
@@ -20,6 +48,8 @@ def update_precinct_election_data(year, election_list, precinct_features):
 
         # Election Data Added As Dictionary Inside Properties Dictionary
         precinct["properties"][year] = election
+
+    print_unmapped_features(precinct_features, year)
 
     return precinct_features
 
@@ -36,6 +66,7 @@ def main():
     election_16_file = None
     election_18_file = None
     precinct_features = None
+    split = None
 
     if args.state_number == 1:
         original_precinct_file = "clean_data/RI_precincts_CLEAN.json"
@@ -45,8 +76,8 @@ def main():
 
     elif args.state_number == 2:
         original_precinct_file = "clean_data/MI_precincts_CLEAN.json"
-        election_16_file = "../original_data/precinct_election_data/Michigan/20161108__mi__general__precinct.csv"
-        election_18_file = "../original_data/precinct_election_data/Michigan/20181106__mi__general__precinct.csv"
+        election_16_file = "../original_data/precinct_election_data/Michigan/2016.csv"
+        election_18_file = "../original_data/precinct_election_data/Michigan/2018.csv"
         updated_precinct_file = "mapped_data/MI_Precincts_MAPPED.geojson"
     elif args.state_number == 3:
         original_precinct_file = "clean_data/NC_VTD_CLEAN.json"
@@ -64,12 +95,23 @@ def main():
     election_18 = pd.read_csv(election_18_file, delimiter=',')
     house_18 = election_18[election_18.office == 'U.S. House']
 
+    #Column Joining     [gave wrong delimiter so redo with , after getting data fresh again
+    # election_16['precinct'] = election_16['precinct'].map(fix_wards_precincts)
+    # election_16.to_csv("2016.csv", sep=',')
+    # exit()
 
-    house_16['precinct'] = house_16['precinct'].str.upper()
-    house_18['precinct'] = house_18['precinct'].str.upper()
+    if args.state_number == 3:
+        house_16['parent_jurisdiction'] = house_16['parent_jurisdiction'].str.upper()
+        house_18['parent_jurisdiction'] = house_18['parent_jurisdiction'].str.upper()
+        split = True
+    else:
+        house_16['precinct'] = house_16['precinct'].str.upper()
+        house_18['precinct'] = house_18['precinct'].str.upper()
+        split = False
 
-    updated_features_16 = update_precinct_election_data("HOUSE_ELECTION_16", house_16, precinct_features)
-    updated_features_both = update_precinct_election_data("HOUSE_ELECTION_18", house_18, updated_features_16)
+
+    updated_features_16 = update_precinct_election_data("HOUSE_ELECTION_16", house_16, precinct_features, split)
+    updated_features_both = update_precinct_election_data("HOUSE_ELECTION_18", house_18, updated_features_16, split)
 
     # write new precinct features to file
     f = open(original_precinct_file, "r")
