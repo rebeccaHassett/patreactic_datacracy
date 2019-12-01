c3292ab0edb09a401a667c7b805feff652f55f68
Andrew Jaffie <andrew+git@ajaffie.dev>
2019-12-01 12:26:35 -0500

    Add endpoint to list districts of state

--- server/src/main/java/edu/sunysb/cs/patractic/datacracy/controller/DistrictDataController.java
+++ server/src/main/java/edu/sunysb/cs/patractic/datacracy/controller/DistrictDataController.java
@@ -1,5 +1,6 @@
 package edu.sunysb.cs.patractic.datacracy.controller;
 
+import edu.stonybrook.politech.annealing.models.concrete.District;
 import edu.sunysb.cs.patractic.datacracy.domain.models.DistrictDataDto;
 import edu.sunysb.cs.patractic.datacracy.domain.models.ElectionId;
 import edu.sunysb.cs.patractic.datacracy.domain.persistence.StateDao;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class DistrictDataController {
@@ -26,5 +30,10 @@ public class DistrictDataController {
         return stateDao.getBaseState(stateName).getDistrict(districtId).dto(new ElectionId(year, type));
     }
 
-
+    @GetMapping(path = "/district/original/{state}")
+    public List<String> getDistrictList(@PathVariable("state") String stateName) {
+        return stateDao.getBaseState(stateName).getDistricts().stream()
+                .map(District::getDistrictId)
+                .collect(Collectors.toList());
+    }
 }
