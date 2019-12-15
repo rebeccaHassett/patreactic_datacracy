90c942a1fae878eacbd5afb4662987cb39342180
dorothyshek <32554289+dorothyshek@users.noreply.github.com>
2019-12-14 20:02:21 -0500

    Update manual_neighbors.py

--- updated_preprocessing/manual_neighbors.py
+++ updated_preprocessing/manual_neighbors.py
@@ -90,6 +90,49 @@ manual_neighbors = {
             "PROVIDENCE 2805"
         ]
     },
-    "MI": {},
+    "MI": {
+        "PEAINE TOWNSHIP 12 Charlevoix County": [
+            "FAIRBANKS TOWNSHIP 1 Delta County",
+            "GARDEN TOWNSHIP 1 Delta County",
+            "THOMPSON TOWNSHIP 1 Schoolcraft County",
+        ],
+        "ST JAMES TOWNSHIP 13 Charlevoix County": [
+            "MUELLER TOWNSHIP 1 Schoolcraft County",
+            "NEWTON TOWNSHIP 1 Mackinac County",
+            "GARFIELD TOWNSHIP 2 Mackinac County",
+            "HUDSON TOWNSHIP 1 Lenawee County"
+        ],
+        "MACKINAC ISLAND CITY 1 Mackinac County": [ # checked
+            "BOIS BLANC TOWNSHIP 1 Mackinac County",
+            "ST. IGNACE CITY 1 Mackinac County",
+            "ST. IGNACE TOWNSHIP 1 Mackinac County",
+        ],
+        "BOIS BLANC TOWNSHIP 1 Mackinac County": [ # checked
+            "BEAUGRAND TOWNSHIP 1 Cheboygan County",
+            "CHEBOYGAN CITY 2 Cheboygan County",
+            "BENTON TOWNSHIP 1 Cheboygan County"
+        ],
+        "DRUMMOND TOWNSHIP 1 Chippewa County": [
+            "DETOUR TOWNSHIP 1 Chippewa County"
+        ],
+        "SUGAR ISLAND TOWNSHIP 1 Chippewa County": [
+            "SAULT STE. MARIE CITY 2 Chippewa County",
+            "SOO TOWNSHIP 1 Chippewa County"
+        ],
+        "GROSSE ILE TOWNSHIP 2 Wayne County": [
+            "TRENTON CITY 1 Wayne County",
+            "RIVERVIEW CITY 1 Wayne County",
+            "WYANDOTTE CITY 3 Wayne County"
+        ],
+        "GROSSE ILE TOWNSHIP 5 Wayne County": [
+            "GIBRALTAR CITY 1 Wayne County",
+            "BROWNSTOWN TOWNSHIP 11 Wayne County",
+            "BROWNSTOWN TOWNSHIP 8 Wayne County"
+        ],
+        "CLAY TOWNSHIP 4 St. Clair County": [
+            "CLAY TOWNSHIP 1 St. Clair County",
+            "CLAY TOWNSHIP 3 St. Clair County"
+        ]
+    },
     "NC": {}
 }
